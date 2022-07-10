b16f491618c330bbd78d24318072a69c1902a45b
Malik Elbadri <elbadrimalik@gmail.com>
2022-07-10 14:38:19 +0300

    first submission

---
+++ script.js
@@ -0,0 +1,10 @@
+const drawer = document.getElementById("drawer");
+
+function openDrawer() {
+    drawer.style.display = "flex";
+}
+
+function closeDrawer() {
+    drawer.style.display = "none";
+
+}
